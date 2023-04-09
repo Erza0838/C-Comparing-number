d222da1b922882d97ffb7766c57f1460ac1aff52
Erza0838 <muhammaderza884@gmail.com>
2023-04-09 13:19:06 +0700

    Belajar overload c#

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OverloadPractice
+{
+    class Overload
+    {
+        static void Main(string[] args)
+        {
+               int inputfirsNumber;
+               int inputsecondNumber;
+
+               InputInteger();
+        }
+
+        // Method untuk melakukan input
+        static int InputInteger(int firsNumber, int secondNumber)
+        {      
+            BiggestNumber inputNumberObject = new BiggestNumber()
+            Console.WriteLine("Masukkan angka untuk dibandingkan : ")
+            Console.ReadLine(firsNumber);
+            Console.ReadLine(secondNumber);
+            Console.WriteLine("Angka yang diinput adalah : " firsNumber, secondNumber);
+        }
+
+        // Method untuk melakukan perbandingan
+        // static int OutputInteger(int firsNumber, int secondNumber)
+        // {      
+        //     BiggestNumber = 
+        //     Console.WriteLine("Angka yang diinput adalah : " firsNumber, secondNumber);
+        // }
+    }
+}
+
